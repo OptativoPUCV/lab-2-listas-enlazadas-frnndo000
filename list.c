7616352e40421cbf16fcf8c93c77d92080a5e44e
Your Name <you@example.com>
2025-03-28 20:04:20 -0300

    new try

--- list.c
+++ list.c
@@ -102,6 +102,23 @@ void pushBack(List * list, void * data) {
 }
 
 void pushCurrent(List * list, void * data) {
+    if (list == NULL || list->current == NULL) return ;
+
+    Node * nuevoNodo = (Node*) malloc(sizeof(Node)) ;
+    if (nuevoNodo == NULL) return ;
+
+    nuevoNodo->data = data ;
+    nuevoNodo->next = list->current->prev ;
+    nuevoNodo->prev = list->current ;
+
+    if (list->current->next != NULL) {
+        list->current->next->prev = nuevoNodo ;
+    } else {
+        list->tail = nuevoNodo ;
+    }
+
+    list->current->next = nuevoNodo ;
+
 }
 
 void * popFront(List * list) {
